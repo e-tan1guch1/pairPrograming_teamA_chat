445f51627a60f37ba9f694e0b539c67f55cc4ffc
id <id@AQ-0437>
2023-06-22 13:33:34 +0900

    変更：UserRepository,LoginController(谷口)

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,13 +1,22 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.repository.UserRepository;
+
 @Controller
 public class LoginController {
+	@Autowired
+	UserRepository userRepository;
+	
 	@GetMapping("/login")
 	public String index() {
 		return "Login";
@@ -17,6 +26,18 @@ public class LoginController {
 	public String login(Model model,
 			@RequestParam(name = "email", required = false) String email,
 			@RequestParam(name = "password", required = false) String password) {
-		return "";
+		
+		String page = "";
+		Optional<User>result = userRepository.findByEmailAndPassword(email,password);
+		
+		User u = null;
+		if(result.isPresent()) {
+			u= result.get();
+			page = "";
+		} else {
+			page = "Login";
+		}
+		
+		return page;
 	}
 }
