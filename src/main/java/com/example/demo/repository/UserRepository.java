e611925d3e461767c189a9d379a216eab22717d3
id <id@AQ-0018>
2023-06-22 14:03:11 +0900

    伊山

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -5,6 +5,6 @@ import java.util.Optional;
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Integer>{
-Optional<User>findByEmailAndPassword(String email,String password);
+public interface UserRepository extends JpaRepository<User, Integer> {
+	Optional<User> findByEmailAndPassword(String email, String password);
 }
