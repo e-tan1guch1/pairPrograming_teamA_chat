19e4db4eb1faacc92930e72de045a9fca900da77
id <id@AQ-0018>
2023-06-22 13:51:05 +0900

    Merge branch 'iyama'

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -11,13 +11,13 @@ import lombok.Data;
 @Entity
 @Table(name = "users")
 public class User {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id; // ユーザID
-	
+
 	private Integer name; // ユーザネーム
-	
+
 	private String email; // メールアドレス
 
 	private String password; // パスワード
