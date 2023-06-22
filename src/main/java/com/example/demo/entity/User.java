f9d600bfad95af87dce8da569d9bc9a2499e0cd5
id <id@AQ-0018>
2023-06-22 13:24:48 +0900

    Merge branch 'master' of https://github.com/e-tan1guch1/pairPrograming_teamA_chat.git

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id; // ユーザID
+
+	private Integer name; // ユーザID
+	
+	private String text; // テキスト
+
+}
