63acb468f5e00afd6f8cc6c686c92f17dfd9a3c8
id <id@AQ-0437>
2023-06-22 13:13:51 +0900

    Login.html変更 LoginController変更 ：谷口

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -9,5 +12,11 @@ public class LoginController {
 	public String index() {
 		return "Login";
 	}
-	
+
+	@PostMapping("/login")
+	public String login(Model model,
+			@RequestParam(name = "email", required = false) String email,
+			@RequestParam(name = "password", required = false) String password) {
+		return "";
+	}
 }
