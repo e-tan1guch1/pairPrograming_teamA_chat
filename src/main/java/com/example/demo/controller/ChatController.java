d7a8e10aceb676a398301b8107c90b2c5084b484
id <id@AQ-0018>
2023-06-22 16:47:29 +0900

    伊山

--- src/main/java/com/example/demo/controller/ChatController.java
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Chat;
@@ -17,8 +18,13 @@ import com.example.demo.model.Display;
 import com.example.demo.repository.ChatRepository;
 import com.example.demo.repository.UserRepository;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class ChatController {
+	
+	@Autowired
+	HttpSession session;
 	
 	@Autowired
 	Account account;
@@ -37,7 +43,7 @@ public class ChatController {
 		List<Display> displays = new ArrayList<>();
 
 		for (Chat chat : chats) {
-			int userId = chat.getId();
+			int userId = account.getId();
 			Optional<User> opt = userRepository.findById(userId);
 			displays.add(new Display(opt.get().getName(), chat.getText()));
 		}
@@ -46,7 +52,7 @@ public class ChatController {
 		return "Chat";
 	}
 	
-	@GetMapping("/chat/add")
+	@PostMapping("/chat/add")
 	public String add(
 			Model m,
 			@RequestParam(name="text", defaultValue="")String text
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ INSERT INTO users(name, email,password) VALUES('田中', 'tanaka@a.com', 'himitu
 
 
 -- チャットテーブルデータ
-INSERT INTO chats(user_id, text) VALUES(1, 'はじめまして');
+INSERT INTO chats(user_id, text, address_id, date) VALUES(1, 'はじめまして', 1, null);
