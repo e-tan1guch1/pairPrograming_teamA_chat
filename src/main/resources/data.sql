0c4561b3c091184398505fe652f5a95fc9ac8347
id <id@AQ-0437>
2023-06-27 09:25:46 +0900

    谷口

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,8 @@
 -- ユーザーテーブルデータ
 INSERT INTO users(name, email,password) VALUES('チャット君', 'chat@a.com', 'himitu');
+INSERT INTO users(name, email,password) VALUES('荒川講師', 'arawaka@mail', 'himitu');
 
 
 -- チャットテーブルデータ
 INSERT INTO chats(user_id, text, address_id, date) VALUES(1, 'ここにメッセージが表示されます', 1, null);
-INSERT INTO chats(user_id, text, address_id, date) VALUES(1, '楽しんで', 1, null);
+INSERT INTO chats(user_id, text, address_id, date) VALUES(1, '左のリストから送信先を選択して、 チャットを始めよう！', 1, null);
