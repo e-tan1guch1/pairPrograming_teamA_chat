509e1b7cfec83e53e692a960ec772df841353637
id <id@AQ-0018>
2023-06-28 17:20:45 +0900

    Merge branch 'master' of https://github.com/e-tan1guch1/pairPrograming_teamA_chat.git

--- src/main/java/com/example/demo/controller/ProfileController.java
+++ src/main/java/com/example/demo/controller/ProfileController.java
@@ -1,9 +1,15 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.User;
 import com.example.demo.model.Account;
@@ -25,6 +31,47 @@ public class ProfileController {
 	
 	@GetMapping("/profile")
 	public String profile(Model m) {
+		
+		User user = userRepository.findById(account.getId()).get();
+		
+		m.addAttribute("user", user);
+		return "profile";
+	}
+	
+	@PostMapping("/profile")
+	public String profile2(Model m,
+			@RequestParam(name="", defaultValue="")String name,
+			@RequestParam(name="", defaultValue="")String email,
+			@RequestParam(name="", defaultValue="")String password
+			) {
+		
+		List<String> error = new ArrayList<>();
+		
+		if (name.equals("") == true) {
+			error.add("名前は必須です");
+
+		}
+		if (email.equals("") == true) {
+			error.add("メールアドレスは必須です");
+
+		} else {
+			Optional<User> opt = userRepository.findByEmail(email);
+			if (opt.isPresent()) {
+				error.add("登録済みのメールアドレスです");
+			}
+		}
+		if (password.equals("") == true) {
+			error.add("パスワードは必須です");
+		}
+		if (error.size() > 0) {
+
+			m.addAttribute("error", error);
+			m.addAttribute("name", name);
+			m.addAttribute("email", email);
+			m.addAttribute("pasword", password);
+		}
+		
+		userRepository.save(new User(account.getId(), name, email, password));
 		
 		User user = userRepository.findById(account.getId()).get();
 		
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -32,6 +32,15 @@ public class User {
 		this.email = email;
 		this.password = password;
 	}
+
+	public User(Integer id, String name, String email, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+	
+	
 	
 
 }
