897f0d0dadfcc39fbe62fd19836d616206fa1a5d
id <id@AQ-0437>
2023-07-05 10:57:13 +0900

    Merge branch 'master' of https://github.com/e-tan1guch1/pairPrograming_teamA_chat.git

--- src/main/java/com/example/demo/controller/todoController.java
+++ src/main/java/com/example/demo/controller/todoController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,100 +36,155 @@ public class todoController {
 
 	@Autowired
 	TodoRepository todoRepository;
-	
+
 	@GetMapping("/todoList")
 	public String list(Model m) {
-//		List<Todo> todos = todoRepository.findAll();
-//		m.addAttribute("todos",todos);
-				
-//		Optional<Todo> opt = todoRepository.findById(account.getId());
-		List<Todo> todo2 = todoRepository.findByUserIdOrderByReleaseDate(account.getId());
-		
-//		for(Todo t : todo2) {
-//			System.err.println(t.getReleaseDate());
-//		}
+		//		List<Todo> todos = todoRepository.findAll();
+		//		m.addAttribute("todos",todos);
 
-		Todo todo =null;
-			
+		//		Optional<Todo> opt = todoRepository.findById(account.getId());
+		List<Todo> todo2 = todoRepository.findByUserIdOrderByReleaseDate(account.getId());
+
+		//		for(Todo t : todo2) {
+		//			System.err.println(t.getReleaseDate());
+		//		}
+
+		Todo todo = null;
+
 		LocalDate tmp = LocalDate.of(1900, 1, 1);
-		
-		
-		for (int i = 0; i<todo2.size(); i++) {
+
+		for (int i = 0; i < todo2.size(); i++) {
 			Todo t = todo2.get(i);
 			LocalDate date = t.getReleaseDate();
-			
-			if (!tmp.toString().equals(date.toString())) {
-				Todo insert = new Todo(0, date, 0,0,"",account.getId());
-				todo2.add(i, insert);
-		
-				tmp = date;
-				System.err.println(tmp);
-			}
+
+			if (date != null) { /*この行エラー対処で記述したけど不要かも*/
+				if (!tmp.toString().equals(date.toString())) {
+					Todo insert = new Todo(0, date, 0, 0, "", account.getId());
+					todo2.add(i, insert);
+
+					tmp = date;
+					System.err.println(tmp);
+				}
+			}/*この行エラー対処で記述したけど不要かも*/
 		}
-		
+
 		for (Todo t : todo2) {
-			System.err.println(t.getId()  + " " + t.getReleaseDate());
+			System.err.println(t.getId() + " " + t.getReleaseDate());
 		}
-//		if (todo2.size() ==0) {
-//			todo2 = null;
-//		}	
-//		List<Todo> list = todoRepository.findByReleaseDate(todo.getReleaseDate());
-		
-		m.addAttribute("todo2",todo2);		
+		//		if (todo2.size() ==0) {
+		//			todo2 = null;
+		//		}	
+		//		List<Todo> list = todoRepository.findByReleaseDate(todo.getReleaseDate());
+
+		m.addAttribute("todo2", todo2);
 		return "todoList";
 	}
-	
 
 	@GetMapping("/todolist/newtodo")
-	public String index() {
+	public String index(
+			@RequestParam(name = "releaseDate", defaultValue = "") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate releaseDate,
+			@RequestParam(name = "hour", required = false) Integer hour,
+			@RequestParam(name = "minute", required = false) Integer minute,
+			@RequestParam(name = "text", required = false) String text,
+			Model m) {
+
+		m.addAttribute("releaseDate", releaseDate);
+		m.addAttribute("hour", hour);
+		m.addAttribute("minute", minute);
+		m.addAttribute("text", text);
+
 		return "todo";
 	}
 
 	@PostMapping("/todolist/newtodo")
 	public String todo(Model m,
-			@RequestParam(name = "releaseDate", defaultValue = "") 
-			@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate releaseDate,
+			@RequestParam(name = "releaseDate", defaultValue = "") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate releaseDate,
 			@RequestParam(name = "hour", required = false) Integer hour,
 			@RequestParam(name = "minute", required = false) Integer minute,
 			@RequestParam(name = "text", required = false) String text) {
 
-//		LocalDateTime timeNow = LocalDateTime.now();
-		
-	Todo todos = new Todo(releaseDate,hour, minute, text,account.getId());
+		//		LocalDateTime timeNow = LocalDateTime.now();
+
+		List<String> error = new ArrayList<>();
+
+		if (releaseDate == null) {
+			error.add("日付は必須です");
+		}
+
+		if (text.equals("") == true) {
+			error.add("本文は必須です");
+
+		}
+
+		if (error.size() > 0) {
+			m.addAttribute("error", error);
+			m.addAttribute("releaseDate", releaseDate);
+			m.addAttribute("hour", hour);
+			m.addAttribute("minute", minute);
+			m.addAttribute("text", text);
+			
+			return "todo";
+		}
+
+		Todo todos = new Todo(releaseDate, hour, minute, text, account.getId());
 		todoRepository.save(todos);
 
 		return "redirect:/todoList";
 	}
-	
+
 	@GetMapping("/todoList/{id}/edit")
-	public String edit(
-			@PathVariable("id")Integer id,
-			Model m) {
+	public String edit(Model m,
+			@PathVariable("id") Integer id,
+			@RequestParam(name = "releaseDate", defaultValue = "") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate releaseDate,
+			@RequestParam(name = "hour", required = false) Integer hour,
+			@RequestParam(name = "minute", required = false) Integer minute,
+			@RequestParam(name = "text", required = false) String text) {
+
 		Todo todo = todoRepository.findById(id).get();
 		m.addAttribute("todo", todo);
-
+		
+//		m.addAttribute("todo.id",todo.getId());
+		
 		return "edit";
 	}
-	
+
 	@PostMapping("/todoList/{id}/edit")
 	public String edit(
 			@PathVariable("id") Integer id,
-			@RequestParam(name = "releaseDate", defaultValue = "") 
-			@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate releaseDate,
+			@RequestParam(name = "releaseDate", defaultValue = "") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate releaseDate,
 			@RequestParam(name = "hour", required = false) Integer hour,
 			@RequestParam(name = "minute", required = false) Integer minute,
 			@RequestParam(name = "text", required = false) String text,
 			Model m) {
-		
-//		LocalDateTime timeNow = LocalDateTime.now();
-		
-//		Todo todos = new Todo(id,timeNow,hour, minute, text);
-		Todo todos = new Todo(id, releaseDate,hour, minute, text,account.getId());
+
+		List<String> error = new ArrayList<>();
+
+		if (releaseDate == null) {
+			error.add("日付は必須です");
+		}
+
+		if (text.equals("") == true) {
+			error.add("本文は必須です");
+
+		}
+
+		if (error.size() > 0) {
+//			m.addAttribute("error", error);
+			m.addAttribute("releaseDate", releaseDate);
+			m.addAttribute("hour", hour);
+			m.addAttribute("minute", minute);
+			m.addAttribute("text", text);
+			
+			return "redirect:/todoList/" + id + "/edit";
+		}
+
+		Todo todos = new Todo(id, releaseDate, hour, minute, text, account.getId());
 		todoRepository.save(todos);
-		
+
 		return "redirect:/todoList";
 	}
 	
+
 	@PostMapping("/todoList/{id}/delete")
 	public String delete(
 			@PathVariable("id") Integer id,
@@ -137,6 +193,5 @@ public class todoController {
 
 		return "redirect:/todoList";
 	}
-	
 
 }
